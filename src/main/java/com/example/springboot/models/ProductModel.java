96676b7f1ac37e24cc714548a52da3484dbc4597
jefferson <antjeffersonbatista@gmail.com>
2023-07-22 22:09:56 -0300

    feat: add post products and add get All products

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
-    private  static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
